02b23ce58c20d0f449c6b69352f89c67c2d66174
natclark <39421782+natclark@users.noreply.github.com>
2021-12-10 04:33:33 -0600

    add google fonts ("Red Rose", "Open Sans") to home page

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,12 @@ export default function Home() {
       <Head>
         <title>Hyperscale - DAO Race</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin />
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Red+Rose:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Red+Rose:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
 
       <div className="flex justify-center">
