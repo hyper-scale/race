42e80c9ac88f8754c2e4fb5fbd78025fed1b0d8a
Lukas <69743171+quick007@users.noreply.github.com>
2021-12-14 23:06:17 -0800

    use tailwind

--- pages/index.js
+++ pages/index.js
@@ -32,22 +32,16 @@ export default function Home({ projects }) {
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <div
+        className="bg-cover bg-no-repeat bg-center"
         style={{
           backgroundImage: "url('/img/top-fold-bg.png')",
-          backgroundSize: "cover",
-          backgroundPosition: "center center",
-          backgroundRepeat: "no-repeat",
         }}
       >
         <div className="flex justify-center">
           <div
-            className="w-screen flex justify-center"
+            className="w-screen flex justify-center max-w-fold-lines bg-no-repeat [backgroundPosition:left_bottom,_right_bottom] [background-size:contain,contain]"
             style={{
               backgroundImage: "url('/img/top-fold-lines-left.png'), url('img/top-fold-lines-right.png')",
-              backgroundSize: "contain, contain",
-              backgroundPosition: "left bottom, right bottom",
-              backgroundRepeat: "no-repeat",
-              maxWidth: "calc(80rem + 200px)",
             }}
           >
             <div className="w-screen max-w-7xl px-4 xl:px-0">
@@ -64,7 +58,7 @@ export default function Home({ projects }) {
       </div>
       <div className="flex justify-center">
         <div className="w-screen max-w-7xl px-4 xl:px-0">
-          <h2 className="text-3xl font-extrabold text-gray-900 text-center mt-12">Trending DAOs</h2>
+          <h2 className="text-3xl font-extrabold text-gray-900 text-center mt-12">Trending DAO</h2>
           <Leaderboard data={projects} numRows={5} />
           <div className="mt-4 text-center">
             <LeaderboardButton />
@@ -77,13 +71,9 @@ export default function Home({ projects }) {
       >
         <div className="flex justify-center">
           <div
-            className="flex justify-center"
+            className="flex justify-center max-w-fold-lines bg-no-repeat [backgroundPosition:left_bottom,_right_bottom] [background-size:contain,contain]"
             style={{
               backgroundImage: "url('/img/top-fold-lines-left.png'), url('img/top-fold-lines-right.png')",
-              backgroundSize: "contain, contain",
-              backgroundPosition: "left bottom, right bottom",
-              backgroundRepeat: "no-repeat",
-              maxWidth: "calc(80rem + 200px)",
             }}
           >
             <div className="w-screen text-center max-w-7xl px-4 xl:px-0 py-16">
@@ -98,7 +88,6 @@ export default function Home({ projects }) {
                 <div className="w-screen max-w-2xl">
                   Communities are a DAO’s most valuable asset. The winning DAO gets funding and joins the HyperscaleDAO.
                   Recieve $200k for 5% by filling out a simple application. You’ll recieve a decision within 1 week.{" "}
-                  {/* <a href="#">Learn More</a> */}
                 </div>
               </div>
               <div className="flex justify-center">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        "fold-lines": "calc(80rem+200px)",
+      }
+    },
   },
   plugins: [],
 };
