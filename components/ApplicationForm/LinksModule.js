e553aa43275d23c51b90c11bff2ee3e7bceafe1a
Kareem Arab <me@kareemarab.com>
2021-12-18 14:02:47 -0500

    added component files

---
+++ components/ApplicationForm/LinksModule.js
@@ -0,0 +1,51 @@
+import { FieldArray, useFormikContext } from "formik";
+import Input from "./Input";
+
+function LinksModule(props) {
+  const { name, label } = props;
+  const { initialValues } = useFormikContext();
+  return (
+    <div className="space-y-3">
+      <div id="links-sections" className="flex flex-col space-y-2">
+        <label htmlFor="email" className="mb-2 font-semibold">
+          {label}
+        </label>
+        <FieldArray key="links" className="w-200">
+          {(arrayHelpers) => {
+            return (
+              <>
+                {initialValues.links.map((_, index) => {
+                  return (
+                    <>
+                      <Input
+                        name={`${name}.${index}`}
+                        placeholder="https://"
+                      />
+                    </>
+                  );
+                })}
+                <div>
+                  <button
+                    onClick={() => {
+                      console.log(arrayHelpers)
+                      arrayHelpers.form.values.links.push('')
+                      arrayHelpers.push({})
+                      console.log(arrayHelpers)
+                    }}
+                    className="p-3 mx-auto border border-indigo-600 rounded-xl text-left font-semibold"
+                    style={{ color: "#5F75EE" }}
+                    type="button"
+                  >
+                    Add link
+                  </button>
+                </div>
+              </>
+            );
+          }}
+        </FieldArray>
+      </div>
+    </div>
+  );
+}
+
+export default LinksModule;
