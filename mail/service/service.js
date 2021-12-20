34e6fa8e5a0fd797b9cef91b236d5cb5bb88e8f9
Aswanth-c <aswanthpcg@gmail.com>
2021-12-21 00:01:28 +0530

    mail service added with sendgrid

--- mail/service/service.js
+++ mail/service/service.js
@@ -1,43 +1,17 @@
-const nodemailer=require("nodemailer");
-const hbs = require('nodemailer-express-handlebars')
-const path = require("path")
-let transporter= nodemailer.createTransport({
-    host:process.env.MAIL_HOST,
-    port:process.env.MAIL_PORT,
-    auth:{
-        user:process.env.MAIL_USER,
-        pass:process.env.MAIL_PASSWORD,
-    },
-    default:{
-        from:process.env.MAIL_FROM,
-    },
-});
-
-const handlebarOptions = {
-    viewEngine: {
-        partialsDir: path.resolve('mail/template/'),
-        defaultLayout: false,
-    },
-    viewPath: path.resolve('mail/template/'),
-};
-
-transporter.use('compile', hbs(handlebarOptions))
-export default async function sendApplicationReceivedMail(mailto,name){
-    let mailOptions = {
-        from: process.env.MAIL_FROM,
-        to: mailto,
-        subject:  'Application Received',
-        template:'applicationreceived',
-        context:{
-            name:name
-        }
-      };
-    await transporter.sendMail(mailOptions,(err,data)=>{
-       if(err){
-        console.log(err)
-       } 
-       else{
-           console.log("mail sent successfully")
-       }
+const sgMail=require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+export default async function sendMailApplicationReceived(mailto){
+    console.log("sent")
+    const msg={
+        to:mailto,
+        from:"downhill.ben@gmail.com",
+        subject: 'Application Received!',
+        text: 'application',
+        html: '<strong> Huge thanks for your Hyperscale application! You will hear from us within 7 days.</strong>',
+    }
+    sgMail.send(msg).then((response)=>{
+        console.log(response[0].statusCode)
+    }).catch((err)=>{
+        console.error(err)
     })
 }
