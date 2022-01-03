3a45aa256f9da93c8ef96d6ff5da0472aa52c1f8
Aswanth-c <aswanthpcg@gmail.com>
2022-01-03 10:45:13 +0530

    added changed in voter

--- components/Voter/Voter.tsx
+++ components/Voter/Voter.tsx
@@ -3,13 +3,15 @@ export interface IVoter {
   image: string;
   power?: any;
 }
-const Voter: React.FunctionComponent<IVoter> = ({ voter, image }: IVoter) => {
+const Voter: React.FunctionComponent<IVoter> = ({ voter, image, power }: IVoter) => {
   return (
     <div className="flex flex-row items-center w-[fit-content]">
       <div>
         <div className="h-8 w-8 bg-gray-300 h-full rounded-full"></div>
       </div>
+      <div className="bg-gray-200 text-sm rounded-full px-2 font-medium text-gray-400 w-[fit-content]">{power}</div>
       <div className="flex-1 px-4 font-semibold text-gray-600">@{voter}</div>
+      <div className="bg-gray-200 text-sm rounded-full px-2 font-medium text-gray-400 w-[fit-content]">{power}</div>
     </div>
   );
 };
